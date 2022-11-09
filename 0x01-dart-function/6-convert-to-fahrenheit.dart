a51ca0b763bb697900803fdcc11a0b6d629440f7
lyndonpett <lyndonpett98@gmail.com>
2022-11-09 10:57:44 -0600

    Adding task 6

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,6 +1,14 @@
 List<double> convertToF(List<double> temperaturesInC) {
   // All the results should be rounded to two decimal points.
-  return temperaturesInC
-      .map((temp) => double.parse((temp * 9 / 5 + 32).toStringAsFixed(2)))
-      .toList();
+  List<double> results = [];
+  for (double temp in temperaturesInC) {
+    double result = double.parse(((temp * 9 / 5) + 32).toStringAsFixed(2));
+
+    // check if result has two decimal points, if not add another decimal point
+    if (result.toString().split('.')[1].length == 2) {
+      result = double.parse(result.toString() + '0');
+    }
+    results.add(result);
+  }
+  return results;
 }

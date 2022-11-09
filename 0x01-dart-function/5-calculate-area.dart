959781d37551fa0b0ac7d68ec1f6cab715aaa9f0
lyndonpett <lyndonpett98@gmail.com>
2022-11-09 10:38:02 -0600

    Adding task 5

--- 0x01-dart-function/5-calculate-area.dart
+++ 0x01-dart-function/5-calculate-area.dart
@@ -1,4 +1,4 @@
 double calculateArea(double height, double base) {
   // The result should be rounded to two decimal places
-  return double.parse((height * base / 2).toStringAsFixed(2));
+  return double.parse((base * height / 2).toStringAsFixed(2));
 }

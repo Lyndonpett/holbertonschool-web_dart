4a2f88a960e51d1088b339a76ce6fc07897fdf31
lyndonpett <lyndonpett98@gmail.com>
2022-11-09 10:20:27 -0600

    Adding task 2

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -10,6 +10,6 @@ int sub(int a, int b)
 
 String showFunc(int a, int b)
 {
-	String str = "Add ${a} + ${b} = ${add(a, b)}\nSub ${a} - ${b} =  ${sub(a, b)}";
+	String str = "Add ${a} + ${b} = ${add(a, b)}\nSub ${a} - ${b} = ${sub(a, b)}";
 	return str;
 }

56b2b178c0f1693e9e38892ddf38a9af65d16020
lyndonpett <lyndonpett98@gmail.com>
2022-11-09 09:54:51 -0600

    Adding task 2

---
+++ 0x00-hello-dart/1-quotes.dart
@@ -0,0 +1,8 @@
+/*
+Write a Dart program that prints exactly “Programming is like building
+a multilingual puzzle, followed by a new line.
+*/
+
+void main() {
+  print('Programming is like building a multilingual puzzle\n');
+}

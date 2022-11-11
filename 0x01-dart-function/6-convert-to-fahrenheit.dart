5b6af9afa20eedd184b30edd163d53c52b17d347
lyndonpett <lyndonpett98@gmail.com>
2022-11-11 09:29:57 -0600

    Reverting

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -13,5 +13,7 @@
 //   return results;
 // }
 List<double> convertToF(List<double> temperaturesInC) {
-  return temperaturesInC.map((temp) => temp * 9 / 5 + 32).toList();
+  return temperaturesInC
+      .map((temp) => double.parse((temp * 9 / 5 + 32).toStringAsFixed(2)))
+      .toList();
 }

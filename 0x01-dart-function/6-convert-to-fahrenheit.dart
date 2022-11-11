f1ecc43595c0b78e48e8d36a9d4b7f2ce02171d4
lyndonpett <lyndonpett98@gmail.com>
2022-11-11 09:28:52 -0600

    Reverting

--- 0x01-dart-function/6-convert-to-fahrenheit.dart
+++ 0x01-dart-function/6-convert-to-fahrenheit.dart
@@ -1,14 +1,17 @@
-List<String> convertToF(List<double> temperaturesInC) {
-  // All the results should be rounded to two decimal points.
-  List<String> results = [];
-  for (double temp in temperaturesInC) {
-    String result = (temp * 9 / 5 + 32).toStringAsFixed(2);
+// List<String> convertToF(List<double> temperaturesInC) {
+//   // All the results should be rounded to two decimal points.
+//   List<String> results = [];
+//   for (double temp in temperaturesInC) {
+//     String result = (temp * 9 / 5 + 32).toStringAsFixed(2);
 
-    // check if result has two decimal points, if not add another decimal point
-    if (result.toString().split('.')[1].length == 1) {
-      result = result + '0';
-    }
-    results.add(result);
-  }
-  return results;
+//     // check if result has two decimal points, if not add another decimal point
+//     if (result.toString().split('.')[1].length == 1) {
+//       result = result + '0';
+//     }
+//     results.add(result);
+//   }
+//   return results;
+// }
+List<double> convertToF(List<double> temperaturesInC) {
+  return temperaturesInC.map((temp) => temp * 9 / 5 + 32).toList();
 }

62df4f1263be202fc1fd4312da42c11371cdc324
lyndonpett <lyndonpett98@gmail.com>
2022-11-14 10:08:43 -0600

    Updated all tasks

--- 0x03-asynchronous/0-users_count.dart
+++ 0x03-asynchronous/0-users_count.dart
@@ -1,6 +1,9 @@
-import "0-main.dart";
 // Create a function usersCount() that prints the number of users.
 // Function prototype : Future<void> usersCount() Gets the user count by calling the provided function fetchUsersCount(). which will simulate fetching data from an api.
+Future<int> fetchUsersCount() => Future.delayed(
+      const Duration(seconds: 2),
+      () => 19,
+    );
 
 Future<void> usersCount() async {
   final int count = await fetchUsersCount();
--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,8 +1,13 @@
-import '1-main.dart';
 import 'dart:convert';
+
 // Create a function getUserID() that returns a future string representing the user’s ID.
 // Function prototype : Future<String> getUserId()
 // Gets the user data by calling the provided function fetchUserData() which will simulate fetching data from an api.
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
 
 Future<String> getUserId() async {
   final String data = await fetchUserData();

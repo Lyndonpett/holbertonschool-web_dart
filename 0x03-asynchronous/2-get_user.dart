94d66e30f4700c8c41c68c23f4c045ec637bb41a
lyndonpett <lyndonpett98@gmail.com>
2022-11-14 10:07:15 -0600

    Task 2?

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,8 +1,15 @@
-import '2-main.dart';
+// import '2-main.dart';
 // Create a function getUser() that prints the user’s string representation.
 // Function prototype : Future<void> getUser()
 // Gets the user data by calling the provided function fetchUserData()
 // Must use : try-catch If an error occurs the function should print “error caught: ”
+Future<String> fetchUserData() =>
+    // Imagine that this function is
+    // more complex and slow.
+    Future.delayed(
+      const Duration(seconds: 2),
+      () => throw 'Cannot locate user',
+    );
 
 Future<void> getUser() async {
   try {

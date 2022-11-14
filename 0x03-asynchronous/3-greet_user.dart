6710c14cc08e09bdb20066b58440bc693f6a0b00
lyndonpett <lyndonpett98@gmail.com>
2022-11-14 10:20:37 -0600

    Adding task 3

--- 0x03-asynchronous/3-greet_user.dart
+++ 0x03-asynchronous/3-greet_user.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import '3-main.dart';
 // Create a function greetUser() that returns a string as follows “hello ”.
 // Function prototype : Future<String> greetUser()
 // Gets the user data by calling the provided function fetchUserData()
@@ -9,15 +10,6 @@ import 'dart:convert';
 // If checkCredentials() returns true loginUser() should call greetUser() and return it’s value
 // If checkCredentials() returns false loginUser() should return “Wrong credentials”
 // Must use try-catch If an error occurs the function should return “error caught: ”
-
-Future<String> fetchUserData() => Future.delayed(
-      const Duration(seconds: 2),
-      () =>
-          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
-    );
-
-Future<bool> checkCredentials() =>
-    Future.delayed(const Duration(seconds: 2), () => true);
 
 Future<String> greetUser() async {
   try {

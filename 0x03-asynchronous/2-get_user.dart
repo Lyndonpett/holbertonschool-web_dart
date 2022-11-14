98a00c13265c93adae034fb2c83df0cdaabd0dee
lyndonpett <lyndonpett98@gmail.com>
2022-11-14 10:03:38 -0600

    Adding task 2

--- 0x03-asynchronous/2-get_user.dart
+++ 0x03-asynchronous/2-get_user.dart
@@ -1,4 +1,4 @@
-import '2-main.dart';
+import "2-main.dart";
 // Create a function getUser() that prints the user’s string representation.
 // Function prototype : Future<void> getUser()
 // Gets the user data by calling the provided function fetchUserData()
@@ -8,6 +8,6 @@ Future<void> getUser() async {
   try {
     print(await fetchUser());
   } catch (e) {
-    print('error caught: $e');
+    print("error caught: $e");
   }
 }

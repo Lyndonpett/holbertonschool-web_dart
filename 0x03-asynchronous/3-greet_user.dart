38113e2109421a97f390a3fa16253451acb0aa1e
lyndonpett <lyndonpett98@gmail.com>
2022-11-14 10:15:19 -0600

    Adding task 3

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,41 @@
+// Create a function greetUser() that returns a string as follows “hello ”.
+// Function prototype : Future<String> greetUser()
+// Gets the user data by calling the provided function fetchUserData()
+// Must use try-catch If an error occurs the function should return “error caught: ”
+// Create a function loginUser()
+// Function prototype : Future<String> loginUser()
+// Should call the provided function checkCredentials() which returns a future bool.
+// If checkCredentials() returns true loginUser() should call greetUser() and return it’s value
+// If checkCredentials() returns false loginUser() should return “Wrong credentials”
+// Must use try-catch If an error occurs the function should return “error caught: ”
+
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<bool> checkCredentials() =>
+    Future.delayed(const Duration(seconds: 2), () => true);
+
+Future<String> greetUser() async {
+  try {
+    final user = await fetchUserData();
+    return 'hello $user';
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
+
+Future<String> loginUser() async {
+  try {
+    final credentials = await checkCredentials();
+    if (credentials) {
+      return greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
